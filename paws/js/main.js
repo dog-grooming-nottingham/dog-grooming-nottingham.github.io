edb40d016a3ca6d3807c3852e8940f941d05f9a5
Martin <martindm@linney.com>
2016-03-11 00:29:30 +0000

    1

---
+++ paws/js/main.js
@@ -0,0 +1,8 @@
+$(window).load(function() { 
+  $('.feedback__collection').bxSlider({
+  	pager: false,
+  	auto: true,
+  	nextText: '<i class="fa fa-angle-right"></i>',
+  	prevText: '<i class="fa fa-angle-left"></i>' 
+	});
+});
