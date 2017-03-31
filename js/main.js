104584d51474648baaedbc6152ab17f7b611a326
Martin <martindm@linney.com>
2017-04-01 00:24:00 +0100

    Thank you page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,24 @@ $(window).load(function() {
 
 	$('body').addClass('loaded'); 
 
+
+// Thank you page message
+var isThanks = function(){
+    return window.location.href.search("[?&]q=") != -1;
+};
+
+if ( isThanks() ) {  
+	var el = document.getElementsByClassName('main-content')[0];
+	el.innerHTML = ''; 
+
+	var p = document.createElement('p');
+	p.innerHTML = "Form sent! We'll be in touch shortly...";
+	p.classList.add("intro");
+	el.appendChild(p);
+} 
+
+// Show bookings available for current month
+
 var monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
@@ -15,8 +33,7 @@ var monthNames = ["January", "February", "March", "April", "May", "June",
 var d = new Date();
 var monthTarget = document.getElementById('month');
 
-if (typeof(monthTarget) != 'undefined' && monthTarget != null)
-	{ monthTarget.innerHTML = monthNames[d.getMonth()]
-	//console.log(monthTarget)
+if (typeof(monthTarget) != 'undefined' && monthTarget != null) 	{ 
+			monthTarget.innerHTML = monthNames[d.getMonth()]
 	}
 });
